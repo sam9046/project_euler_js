8bf575613d8314baddc3f7c2466eecc7e372e85a
Sam Cackett <samcackett@uk.ibm.com>
2017-11-18 18:52:43 +0000

    Remove unnecessary check for 0

--- spec/001/src.js
+++ spec/001/src.js
@@ -6,7 +6,7 @@ const MULTIPLESOF3AND5 = function() {
     MULTIPLESOF3AND5.prototype.multiple = function (value) {
         var result = [];
         for(var i = 0; i < value; i++) {
-            if(i > 0 && (i % 3 === 0 || i % 5 === 0))
+            if(i % 3 === 0 || i % 5 === 0)
                 result.push(i);
         }
         return this.sumArray(result);
